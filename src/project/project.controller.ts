27d1a66b3f4d938a48fc49e776d2620dfb395529
SkyMatix44 <timschmitt97.ts@gmail.com>
2022-05-06 19:56:35 +0200

    add endpoint getProjectUser

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Request, UseGuards } from '@nestjs/common';
-import { Project, Role } from '@prisma/client';
+import { Project, Role, User } from '@prisma/client';
 import { Roles } from '../auth/decorator';
 import { RolesGuard } from '../auth/guard/roles.guard';
 import { TrackingRequest } from '../auth/middleware/auth.middleware';
@@ -33,14 +33,23 @@ export class ProjectController {
   }
 
   @Get()
-  getProjects(@Request() req: TrackingRequest, @Body() dto: CreateProjectDto): Promise<Project[]> {
+  getProjects(@Request() req: TrackingRequest): Promise<Project[]> {
     return this.projectService.getProjectsOfUser(req);
   }
 
   @Roles(Role.ADMIN, Role.SCIENTIST)
   @UseGuards(RolesGuard)
   @Get('all')
-  getAllProjects(@Request() req: TrackingRequest, @Body() dto: CreateProjectDto): Promise<Project[]> {
+  getAllProjects(@Request() req: TrackingRequest): Promise<Project[]> {
     return this.projectService.getAllProjects(req);
+  }
+
+  @Get(':id/users/:option')
+  getProjectUsers(
+    @Request() req: TrackingRequest,
+    @Param('id', ParseIntPipe) projectId: number,
+    @Param('option') option: 'all' | 'participants' | 'scientists',
+  ): Promise<User[]> {
+    return this.projectService.getProjectUsers(req, projectId, option);
   }
 }
